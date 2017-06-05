b7c900c77560ea2fc882eea4a93aa7c5e4ff7801
Mark <aloNEWolf5267@gmail.com>
2017-06-05 11:33:41 -0700

    Add files via upload

---
+++ NodeMCU_ESP8266/Client.ino
@@ -0,0 +1,160 @@
+//------------------------------------------------------------------------------------
+// Libraries Needed For This Project
+//------------------------------------------------------------------------------------
+  #include <EEPROM.h>         // To Be Able To Store & Read Data Between Power Off
+  #include <ESP8266WiFi.h>    // The Basic Function Of The ESP NOD MCU
+//------------------------------------------------------------------------------------
+// Defining I/O Pins
+//------------------------------------------------------------------------------------
+    #define D0 16   // On Board LED [ RED ]
+    #define D1 5
+    #define D2 4
+    #define D3 0
+    #define D4 2    // WIFI Module LED [ BLUE ]
+    #define D5 14
+    #define D6 12
+    #define D7 13        
+//------------------------------------------------------------------------------------
+// Variables
+//------------------------------------------------------------------------------------
+  String Data;
+  const String  Name = "NodeMCU_Client_1";
+//------------------------------------------------------------------------------------
+// WIFI Authentication Variables
+//------------------------------------------------------------------------------------
+/* This Client Is Going To Connect To A WIFI Server Access Point
+ * So You Have To Specify Server WIFI SSID & Password In The Code Not Here
+ * Please See The Function Below Name (WiFi.Begin)
+ * If WIFI dont need Any password Then WiFi.begin("SSIDNAME")
+ * If WIFI needs a Password Then WiFi.begin("SSIDNAME", "PASSWORD")
+ */
+  char*         TKDssid;            // Wifi Name
+  char*         TKDpassword;        // Wifi Password
+//------------------------------------------------------------------------------------
+// WIFI Module Role & Port
+//------------------------------------------------------------------------------------
+/* This WIFI Module Code Works As A Client
+ * That Will Connect To A Server WIFI Modul With (IP ADDress 192.168.4.1)
+ */
+  int             TKDServerPort  = 9001;
+  IPAddress       TKDServer(192,168,4,1);
+  WiFiClient      TKDClient;
+//====================================================================================
+
+  void setup() 
+  {
+    // Setting The Serial Port ----------------------------------------------
+    Serial.begin(115200);           // Computer Communication
+    
+    // Setting The Mode Of Pins ---------------------------------------------
+    pinMode(D4, OUTPUT);          // WIFI OnBoard LED Light
+    pinMode(D0, OUTPUT);          //On Board
+    digitalWrite(D4, HIGH);       // Turn WiFi LED Off
+    digitalWrite(D0, LOW);
+    
+    // Print Message Of I/O Setting Progress --------------------------------
+    Serial.println("\nI/O Pins Modes Set .... Done");
+
+    // Starting To Connect --------------------------------------------------
+    if(WiFi.status() == WL_CONNECTED)
+    {
+      WiFi.disconnect();
+      WiFi.mode(WIFI_OFF);
+      delay(50);
+    }
+
+    /* in this part it should load the ssid and password 
+     * from eeprom they try to connect using them */
+    
+    WiFi.mode(WIFI_STA);            // To Avoid Broadcasting An SSID
+    WiFi.begin("NodeMCU_Server", "");      // The SSID That We Want To Connect To
+
+    // Printing Message For User That Connetion Is On Process ---------------
+    Serial.println("!--- Connecting To " + WiFi.SSID() + " ---!");
+
+    // WiFi Connectivity ----------------------------------------------------
+    CheckWiFiConnectivity();        // Checking For Connection
+
+    // Stop Blinking To Indicate Connected ----------------------------------
+    digitalWrite(D4, !HIGH);
+    Serial.println("!-- Client Device Connected --!");
+
+    // Printing IP Address --------------------------------------------------
+    Serial.println("Connected To      : " + String(WiFi.SSID()));
+    Serial.println("Signal Strenght   : " + String(WiFi.RSSI()) + " dBm");
+    Serial.print  ("Server IP Address : ");
+    Serial.println(TKDServer);
+    Serial.print  ("Server Port Num   : ");
+    Serial.println(TKDServerPort);
+    // Printing MAC Address
+    Serial.print  ("Device MC Address : ");
+    Serial.println(String(WiFi.macAddress()));
+    // Printing IP Address
+    Serial.print  ("Device IP Address : ");
+    Serial.println(WiFi.localIP());
+    
+    // Connecting The Device As A Client -------------------------------------
+    TKDRequest();
+  }
+
+//====================================================================================
+  
+  void loop()
+  {
+
+    Read_Data_And_Send();
+    CheckWiFiConnectivity();
+  
+  }
+
+//====================================================================================
+
+  void Read_Data_And_Send()
+  {
+    if(Serial.available() > 0) // Don't read unless there is no data
+    {
+      // Reading The Data
+      Data = Serial.readString();
+      
+      Serial.println( Name + ": " + Data ); // printing the data in the Client serial monitor 
+      TKDClient.println( Data ); // printing the data in the Server serial monitor
+      TKDClient.flush();
+    }
+  }
+
+//====================================================================================
+
+  void CheckWiFiConnectivity()
+  {
+    while(WiFi.status() != WL_CONNECTED)
+    {
+      for(int i=0; i < 10; i++)
+      {
+        digitalWrite(D4, !HIGH);
+        delay(250);
+        digitalWrite(D4, !LOW);
+        delay(250);
+        Serial.print(".");
+      }
+      Serial.println("");
+      digitalWrite(D0, HIGH);
+    }
+    digitalWrite(D0, LOW);
+  }
+        
+//====================================================================================
+
+  void TKDRequest()
+  {
+    // First Make Sure You Got Disconnected
+    TKDClient.stop();
+
+    // If Sucessfully Connected Send Connection Message
+    if(TKDClient.connect(TKDServer, TKDServerPort))
+    {
+      Serial.println    ("<" + Name + "-CONNECTED>");
+      TKDClient.println ("<" + Name + "-CONNECTED>");
+    }
+  }
+
+//====================================================================================
---
+++ NodeMCU_ESP8266/Server.ino
@@ -0,0 +1,182 @@
+//------------------------------------------------------------------------------------
+// Libraries Needed For This Project
+//------------------------------------------------------------------------------------
+  #include <ESP8266WiFi.h>
+//------------------------------------------------------------------------------------
+// Defining I/O Pins
+//------------------------------------------------------------------------------------
+    #define D0 16  //On Board LED [ RED ]
+    #define D1 5
+    #define D2 4
+    #define D3 0
+    #define D4 2   // WIFI Module LED [ BLUE ]
+    #define D5 14
+    #define D6 12
+    #define D7 13     
+//------------------------------------------------------------------------------------
+  // Authentication Variables
+  char*       TKDssid;              // SERVER WIFI NAME
+  char*       TKDpassword;          // SERVER PASSWORD
+//------------------------------------------------------------------------------------
+  #define     MAXSC     1           // MAXIMUM NUMBER OF CLIENTS
+  String Message
+
+  /*
+   * MAXSC => Maximum Server Clients
+   * They Are 6 Because
+   * 1. Blue Fighter Suit
+   * 2. Red Fighter Suit
+   * 3. Corner Referee 1
+   * 4. Corner Referee 2
+   * 5. Corner Referee 3
+   * 6. Corner Referee 4
+   */
+  
+  WiFiServer  TKDServer(9001);      // THE SERVER AND THE PORT NUMBER
+  WiFiClient  TKDClient[MAXSC];     // THE SERVER CLIENTS (Devices)
+//=====Setup=====
+
+  void setup()
+  {
+    // Setting The Serial Port
+    Serial.begin(115200);           // Computer Communication
+    
+    pinMode(D4, OUTPUT);          // WIFI OnBoard LED Light
+    pinMode(D0, OUTPUT);          //On Board
+    digitalWrite(D4, HIGH);       // Turn WiFi LED Off
+    digitalWrite(D4, LOW);        // Turn on Board LED On 
+  
+    // Print Message Of I/O Setting Progress
+    Serial.println();
+    Serial.println("I/O Pins Modes Set .... Done");
+
+    // Setting Up A Wifi Access Point
+    SetWifi("NodeMCU_Server", "");
+  }
+
+//====Loop====
+  
+  void loop()
+  {
+    // Checking For Available Clients
+    AvailableClients();
+    // Checking For Available Client Messages
+    AvailableMessage();
+    // 
+  }
+
+//==========Setting Up Access Point==========
+  
+  void SetWifi(char* Name, char* Password)
+  {
+    // Stop Any Previous WIFI
+    WiFi.disconnect();
+
+    // Setting The Wifi Mode
+    WiFi.mode(WIFI_AP_STA);
+    Serial.println("WIFI Mode : AccessPoint Station");
+    
+    // Setting The AccessPoint Name & Password
+    TKDssid      = Name;
+    TKDpassword  = Password;
+    
+    // Starting The Access Point
+    WiFi.softAP(TKDssid, TKDpassword);
+    Serial.println("WIFI < " + String(TKDssid) + " > ... Started");
+    
+    // Wait For Few Seconds
+    delay(100);
+    
+    // Getting Server IP
+    IPAddress IP = WiFi.softAPIP();
+    
+    // Printing The Server IP Address
+    Serial.print("AccessPoint IP : ");
+    Serial.println(WiFi.softAPIP());
+
+    // Printing MAC Address
+    Serial.print("AccessPoint MC : ");
+    Serial.println(WiFi.softAPmacAddress());
+
+    // Starting Server
+    TKDServer.begin();
+    TKDServer.setNoDelay(true);
+    Serial.println("Server Started");
+  }
+
+//=====Connected Clients=====
+
+  void AvailableClients()
+  {   
+    if (TKDServer.hasClient())
+    {
+      // Read D4 Switch To Low If High.
+      if(digitalRead(D4) == HIGH) digitalWrite(D4, LOW);
+      
+      for(uint8_t i = 0; i < MAXSC; i++)
+      {
+        //find free/disconnected spot
+        if (!TKDClient[i] || !TKDClient[i].connected())
+        {
+          // Checks If Previously The Client Is Taken
+          if(TKDClient[i])
+          {
+            TKDClient[i].stop();
+          }
+
+          // Checks If Clients Connected To The Server
+          if(TKDClient[i] = TKDServer.available())
+          {
+            Serial.println("New Client: " + String(i));
+          }
+
+          // Continue Scanning
+          continue;
+        }
+      }
+      
+      //no free/disconnected spot so reject
+      WiFiClient TKDClient = TKDServer.available();
+      TKDClient.stop();
+    }
+    else
+    {
+      // This LED Blinks If No Clients Where Available
+      digitalWrite(D4, HIGH);
+      delay(250);
+      digitalWrite(D4, LOW);
+      delay(250);
+    }
+  }
+
+//=====Message From Connected Clients=====
+
+  void AvailableMessage()
+  {
+    //check clients for data
+    for(uint8_t i = 0; i < MAXSC; i++)
+    {
+      if (TKDClient[i] && TKDClient[i].connected() && TKDClient[i].available())
+      {
+          while(TKDClient[i].available())
+          {
+            Message = TKDClient[i].readStringUntil('\n');
+            TKDClient[i].flush();
+            Serial.println("NodeMCU_Client_" + String(i) + " : " + Message);
+          }
+      }
+    }
+  }
+
+//======Command Reconstruction=====
+
+ void CommandRecieved()
+ {
+    //The first 4 bits decide position of the Motor_1
+    //The second 4 bits decide position of the Motor_2
+    //The third 4 bits decide position of the Motor_3
+    //The fourth 4 bits decide position of the Motor_4
+
+}
+
+//==================End==================
